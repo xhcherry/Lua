b2453710f871d247f3ef2ae0c71366d64b3ee592
xhcherry <83774342+xhcherry@users.noreply.github.com>
2022-09-18 22:24:57 +0800

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -1,11 +1,10 @@
---[[
 print("hello")
 
 function say()
     print("asd")
 end
 say()
-
+print()
 function max(a, b)
     if a > b then
         return a
@@ -14,37 +13,7 @@ function max(a, b)
     end
 end
 print(max(2, 3))
-
+print()
 for i=1,100 do
     print(i)
 end
---]]
-
---[[
-Config = {h="hello"}
-Config.word="hello"
-Config.num="100"
-Config["name"]="zhang"
-print(Config["num"])
-print(Config.word)
-
-for i, v in pairs(Config) do
-    print(i,v)
-end
---]]
-
---arr={1,2,3,4,5,"sdf"}
---for i, v in pairs(arr) do
---   print(i,v)
---end
-
---[[arr = {}
-for i = 1, 100 do
-    table.insert(arr, 1, i)
-end
-for i, v in pairs(arr) do
-    print(i, v)
-end--]]
-
-
-
